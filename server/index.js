cd8dda8864c1a73e4b3c48397ae47589b564004c
vignesh-yadav <vigneshyadav.dev@gmail.com>
2025-10-31 19:27:31 +0530

    fix: update React and ReactDOM to version 18.3.1; set server port to use environment variable

--- server/index.js
+++ server/index.js
@@ -35,7 +35,8 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
